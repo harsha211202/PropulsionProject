919c9d31beeaa6070b33fb39bbe3c87fb6fce813
AshtonVandiver <ashtonvandiver@gmail.com>
2024-12-03 13:42:06 -0600

    Sudo Code

--- CycleAnalysis.m
+++ CycleAnalysis.m
@@ -1,1 +1,23 @@
+%% Runs the Cycle Analysis for Our Mach 2 Turbojet at 14000 M
+
 C = Constants
+
+%% Inlet
+
+%% Compressor
+
+%% Combustor
+
+%% Turbine 
+
+%% Afterburner
+
+%% Nozzle
+
+%% Plots
+
+% Plot of Engine Thrust vs Mach
+
+% Plot of Engine Thrust vs Alt @ Mach 2
+
+% Contour Plots/ Plots of Constant Line for Engine Thrust with Alt + Mach
