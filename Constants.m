f19cfa5bc44c61b785ed3f8230b1f2124631081f
AshtonVandiver <ashtonvandiver@gmail.com>
2024-12-03 13:35:47 -0600

    Updated Constants

--- Constants.m
+++ Constants.m
@@ -1,3 +1,13 @@
-function C = constants
-	
+function C = constants()
+	%% Flight Conditions
+	C.M0 = 2; % Cruise Mach Number
+	C.Alt = 14000; % Cruise Alt, Meters
+
+	%% Vehicle
+	C.Pay = 10000; % Payload, kg
+	C.Range = 2500; % Range, km
+
+	%% Engine Requirements
+
+
 end
